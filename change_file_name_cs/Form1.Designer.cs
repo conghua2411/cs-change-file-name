f6eff67414c710b94db2e2e5c3932cf7aae94eb4
hoangcong <conghua2411@gmail.com>
2018-07-08 21:11:50 +0700

    upload project 2

---
+++ change_file_name_cs/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace change_file_name_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb_dir = new System.Windows.Forms.TextBox();
+            this.btn_searchDir = new System.Windows.Forms.Button();
+            this.btn_change = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.cb_hasChar = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_recursion = new System.Windows.Forms.CheckBox();
+            this.cb_nameLength = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // tb_dir
+            // 
+            this.tb_dir.Location = new System.Drawing.Point(12, 44);
+            this.tb_dir.Name = "tb_dir";
+            this.tb_dir.Size = new System.Drawing.Size(179, 20);
+            this.tb_dir.TabIndex = 0;
+            // 
+            // btn_searchDir
+            // 
+            this.btn_searchDir.Location = new System.Drawing.Point(197, 44);
+            this.btn_searchDir.Name = "btn_searchDir";
+            this.btn_searchDir.Size = new System.Drawing.Size(75, 23);
+            this.btn_searchDir.TabIndex = 1;
+            this.btn_searchDir.Text = "folder dir";
+            this.btn_searchDir.UseVisualStyleBackColor = true;
+            this.btn_searchDir.Click += new System.EventHandler(this.btn_searchDir_Click);
+            // 
+            // btn_change
+            // 
+            this.btn_change.Location = new System.Drawing.Point(52, 214);
+            this.btn_change.Name = "btn_change";
+            this.btn_change.Size = new System.Drawing.Size(75, 23);
+            this.btn_change.TabIndex = 2;
+            this.btn_change.Text = "change";
+            this.btn_change.UseVisualStyleBackColor = true;
+            this.btn_change.Click += new System.EventHandler(this.btn_change_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(168, 214);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 3;
+            this.btn_exit.Text = "exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // cb_hasChar
+            // 
+            this.cb_hasChar.AutoSize = true;
+            this.cb_hasChar.Location = new System.Drawing.Point(12, 153);
+            this.cb_hasChar.Name = "cb_hasChar";
+            this.cb_hasChar.Size = new System.Drawing.Size(91, 17);
+            this.cb_hasChar.TabIndex = 5;
+            this.cb_hasChar.Text = "has character";
+            this.cb_hasChar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Name length :";
+            // 
+            // cb_recursion
+            // 
+            this.cb_recursion.AutoSize = true;
+            this.cb_recursion.Location = new System.Drawing.Point(12, 176);
+            this.cb_recursion.Name = "cb_recursion";
+            this.cb_recursion.Size = new System.Drawing.Size(103, 17);
+            this.cb_recursion.TabIndex = 9;
+            this.cb_recursion.Text = "Recursion folder";
+            this.cb_recursion.UseVisualStyleBackColor = true;
+            // 
+            // cb_nameLength
+            // 
+            this.cb_nameLength.FormattingEnabled = true;
+            this.cb_nameLength.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.cb_nameLength.Location = new System.Drawing.Point(91, 127);
+            this.cb_nameLength.Name = "cb_nameLength";
+            this.cb_nameLength.Size = new System.Drawing.Size(45, 21);
+            this.cb_nameLength.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.cb_nameLength);
+            this.Controls.Add(this.cb_recursion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_hasChar);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_change);
+            this.Controls.Add(this.btn_searchDir);
+            this.Controls.Add(this.tb_dir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change name file";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_dir;
+        private System.Windows.Forms.Button btn_searchDir;
+        private System.Windows.Forms.Button btn_change;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.CheckBox cb_hasChar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cb_recursion;
+        private System.Windows.Forms.ComboBox cb_nameLength;
+    }
+}
+
---
+++ change_file_name_cs/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace change_file_name_cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_searchDir_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                tb_dir.Text = fbd.SelectedPath;
+            }
+        }
+
+        private static Random rnd = new Random();
+        // fileName : abc.xyz
+        private string randomNameFileType(string file, int length, bool hasChar)
+        {
+            string newName = "";
+
+            if (hasChar)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (rnd.Next(2) == 0)
+                    {
+                        newName += ((char)(rnd.Next(26) + 97)).ToString();
+                    }
+                    else
+                    {
+                        newName += rnd.Next(10).ToString();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    newName += rnd.Next(10).ToString();
+                }
+
+            }
+
+            string fileName = file.Substring(0,file.IndexOf("."));
+            string fileType = file.Substring(file.LastIndexOf("."));
+
+            return newName + fileType;
+        }
+
+        private void changeNameInFolder(string folderDir, int length, bool hasChar)
+        {
+            string[] files = Directory.GetFiles(folderDir);
+
+            string tmpName, newName = "";
+
+            for (int i = 0; i < files.Length; i++)
+			{
+                newName = folderDir + "\\" + randomNameFileType(files[i].Substring(files[i].LastIndexOf("\\")), length, hasChar);
+                
+                if (!File.Exists(newName))
+                {
+                    System.IO.File.Move(files[i], newName);
+                }
+                else
+                {
+                    tmpName = newName.Substring(newName.LastIndexOf("\\") +1);
+
+                    int j = 1;
+
+                    tmpName = tmpName.Insert(tmpName.LastIndexOf("."), " (0)");
+
+                    do
+                    {
+                        tmpName = tmpName.Replace("(" + (j - 1).ToString() + ")", "(" + j.ToString() + ")");
+                        newName = folderDir + "\\" + tmpName;
+
+                        j++;
+
+                    } while (File.Exists(newName));
+
+                    System.IO.File.Move(files[i], newName);
+                }
+            }
+        }
+
+        private void changeFolderRecursion(string folderDir, int length, bool hasChar)
+        {
+            changeNameInFolder(folderDir, length, hasChar);
+            string[] subFolder = Directory.GetDirectories(folderDir);
+
+            foreach (string item in subFolder)
+            {
+                changeFolderRecursion(item, length, hasChar);
+            }
+        }
+
+        private Boolean checkTextBoxLength(String tbLength)
+        {
+            if (string.IsNullOrEmpty(tbLength))
+            {
+                return false;
+            }
+
+            // isdigit
+            // 48:0            57:9
+            foreach (char item in tbLength)
+            {
+                if (item < 48 || item > 57)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void btn_change_Click(object sender, EventArgs e)
+        {
+            if (!checkTextBoxLength(cb_nameLength.Text))
+            {
+                MessageBox.Show("length are wrong");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(tb_dir.Text))
+            {
+
+                string[] files = Directory.GetFiles(tb_dir.Text);
+
+                string[] subFolder = Directory.GetDirectories(tb_dir.Text);
+
+
+                int length = Int32.Parse(cb_nameLength.Text);
+
+                if (cb_recursion.Checked)
+                {
+                    changeFolderRecursion(tb_dir.Text, length, cb_hasChar.Checked);
+                }
+                else
+                {
+                    changeNameInFolder(tb_dir.Text, length, cb_hasChar.Checked); 
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Directory folder is empty");
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
